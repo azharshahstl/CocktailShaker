5a28d1f29a8fcd9a877d8428af7c8ae7c5d18be1
azharshahstl <azharshahstl@yahoo.com>
2020-02-11 16:31:46 -0600

    updated seed file and got collection select working on cocktails

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 martini = Glassware.create(style: "Martini Glass")
+coupe = Glassware.create(style: "Coupe")
+flute = Glassware.create(style: "Flute")
+highball = Glassware.create(style: "Highball")
+short_rocks = Glassware.create(style: "Short Rocks Glass")
+tall_rocks = Glassware.create(style: "Tall Rocks Glass")
+
+dry = Style.create(pallette: "Dry")
+dry = Style.create(pallette: "Semi-Dry")
+dry = Style.create(pallette: "Sweet")
+
+olives = Garnish.create(kind: "Oilves")
+lemon_twist = Garnish.create(kind: "Lemon Twist")
+orange_twist = Garnish.create(kind: "Orange Twist")
+cherry = Garnish.create(kind: "Luxardo Cherry")
+flowers = Garnish.create(kind: "Flowers")
